bb025c1957788ee0c5e0a3231987fd271c41bda4
Lalubaba <ansh.uchandaliya8@gmail.com>
2024-11-15 18:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY SHARP
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("6793647608:AAFZ5pjbYsqGq_S5nbBEDO1o6s_eFbxsM_I")
+BOT_TOKEN=("8091709404:AAH_TwGTOuNw6iyECX_QXqrhY1f4zu2WbfU")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("SharpX72")
+OWNER_USERNAME=("@ITS_TUFAN_01")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1009132250")
+ADMIN_IDS=("7825026453")
 
 
 
