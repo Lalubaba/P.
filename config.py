884941a4e29f76ff06c2cea11caa5d1100304b1a
Lalubaba <ansh.uchandaliya8@gmail.com>
2024-11-15 17:06:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6793647608:AAFZ5pjbYsqGq_S5nbBEDO1o6s_eFbxsM_I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SharpX72")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1009132250")
+
+
+
+
+
+
+
+  
